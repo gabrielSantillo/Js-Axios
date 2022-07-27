2780281c49d62f3fbcdec7fcf23062961b5ded4b
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-27 17:41:04 -0600

    Added a new functionality to the code, getting infirmation from the API with params

--- js/app.js
+++ js/app.js
@@ -1,175 +1,88 @@
-/* this function get an aleatory course from an API based on the education category */
-function educationActivity(details) {
-  /* getting the section from the page */
-  let section = document.querySelector(`section`);
-  /* this function insert onto the page the type of category was chosen, its activity, price and link the course */
-  function post_success(response) {
-    section.insertAdjacentHTML(
-      `beforeend`,
-      `<div>
-        <h1>${response[`data`][`type`]}</h1>
-        <h2>${response[`data`][`activity`]}</h2>
-        <p>$ ${response[`data`][`price`]}</p>
-        <a href=${response[`data`][`link`]}>Link to course</a>
-        </div>
+/* this function display all the options the user has to chose between a range of courses */
+function displayActivity(details) {
+    /* inserting the option in the screen */
+  document.querySelector(`main`).insertAdjacentHTML(
+    `beforeend`,
+    `
+        <button id="education_button">Education</button>
+        <button id="recreational_button">Recreational</button>
+        <button id="social_button">Social</button>
+        <button id="diy_button">Diy</button>
+        <button id="charity_button">Charity</button>
+        <button id="cooking_button">Cooking</button>
+        <button id="relaxation_button">Relaxation</button>
+        <button id="music_button">Music</button>
+        <button id="busywork_button">Busywork</button>
+        <hr>
         `
-    );
-  }
+  );
 
-  /* in case the request fail, an error message will be inserted to the page */
-  function post_failure(error) {
-    main.insertAdjacentHTML(
-      `beforeend`,
-      `
+  /* getting the first button ny its IS */
+  let education_button = document.getElementById(`education_button`);
+  /* after the button being clicked, call the function addFreeOrPaidOptions */
+  education_button.addEventListener(`click`, addFreeOrPaidOptions);
+}
+
+/* this function simply insert in the page two options for the user to chose, free or paid courses */
+function addFreeOrPaidOptions(response) {
+    /* adding in the pages to buttons, free and paid options */
+    main.insertAdjacentHTML(`beforeend`, `
+    <button id="free_button">Free Courses</button>
+    <button id="paid_button">Paid Courses</button>
+    `);
+
+    /* getting the free button from the page */
+    let free_button = document.getElementById(`free_button`);
+    /* after being clicked, call the function freeEducationActivity */
+    free_button.addEventListener(`click`, freeEducationActivity);
+}
+
+/* this function is called when the success occurs when requesting information form the API */
+function post_success(response) {
+    /* inserting onto the page informatio about the course */
+  main.insertAdjacentHTML(
+    `beforeend`,
+    /* inserting the type of course, description and link*/
+    `
+    <h3>${response[`data`][`type`]}</h3>
+    <p>${response[`data`][`activity`]}</p>
+    <a href=${response[`data`][`link`]}>Link to the course</a>
+        `
+  );
+
+}
+
+/* when the request function fail, an error message is printed */
+function post_failure(error) {
+  main.insertAdjacentHTML(
+    `beforeend`,
+    `
           <h1>ERROR</h1>
           `
-    );
-  }
+  );
+}
 
-  /* making request to an API already defined by its category */
+/* this function make a request to the api by type and price */
+function freeEducationActivity(details) {
   axios
     .request({
+        /* getting from the API, only options from the education category */
       url: `http://www.boredapi.com/api/activity?type=education`,
+      /* adding a param to only get free option */
+      params: {
+        price: 0
+      }
     })
-    /* in case the request was successful, call the function post_success */
+    /* if the request is successful, call the function post_success */
     .then(post_success)
-    /* in case the request wasn't successful, call the function post_failure */
+    /* if the request fail, call the function post_failure */
     .catch(post_failure);
 }
 
-/* getting the button from the page */
-let education_button = document.getElementById(`education_button`);
-/* after the button being clicked, call the function educationActivity */
-education_button.addEventListener(`click`, educationActivity);
+/* global variable that gets the main tag */
+let main = document.querySelector(`main`);
 
-/* this function get an aleatory course from an API based on the recreational category */
-function recreationalActivity(details) {
-  /* getting the section from the page */
-  let section = document.querySelector(`section`);
-  /* this function insert onto the page the type of category was chosen, its activity, price and link the course */
-  function post_success(response) {
-    section.insertAdjacentHTML(
-      `beforeend`,
-      `<div>
-          <h1>${response[`data`][`type`]}</h1>
-          <h2>${response[`data`][`activity`]}</h2>
-          <p>$ ${response[`data`][`price`]}</p>
-          <a href=${response[`data`][`link`]}>Link to course</a>
-          </div>
-          `
-    );
-  }
-
-  /* in case the request fail, an error message will be inserted to the page */
-  function post_failure(error) {
-    main.insertAdjacentHTML(
-      `beforeend`,
-      `
-            <h1>ERROR</h1>
-            `
-    );
-  }
-
-  /* making request to an API already defined by its category */
-  axios
-    .request({
-      url: `http://www.boredapi.com/api/activity?type=recreational`,
-    })
-    /* in case the request was successful, call the function post_success */
-    .then(post_success)
-    /* in case the request wasn't successful, call the function post_failure */
-    .catch(post_failure);
-}
-
-/* getting the button from the page */
-let recreational_button = document.getElementById(`recreational_button`);
-/* after the button being clicked, call the function educationActivity */
-recreational_button.addEventListener(`click`, recreationalActivity);
-
-/* this function get an aleatory course from an API based on the social category */
-function socialActivity(details) {
-  /* getting the section from the page */
-  let section = document.querySelector(`section`);
-  /* this function insert onto the page the type of category was chosen, its activity, price and link the course */
-  function post_success(response) {
-    section.insertAdjacentHTML(
-      `beforeend`,
-      `<div>
-            <h1>${response[`data`][`type`]}</h1>
-            <h2>${response[`data`][`activity`]}</h2>
-            <p>$ ${response[`data`][`price`]}</p>
-            <a href=${response[`data`][`link`]}>Link to course</a>
-            </div>
-            `
-    );
-  }
-
-  /* in case the request fail, an error message will be inserted to the page */
-  function post_failure(error) {
-    main.insertAdjacentHTML(
-      `beforeend`,
-      `
-              <h1>ERROR</h1>
-              `
-    );
-  }
-
-  /* making request to an API already defined by its category */
-  axios
-    .request({
-      url: `http://www.boredapi.com/api/activity?type=social`,
-    })
-    /* in case the request was successful, call the function post_success */
-    .then(post_success)
-    /* in case the request wasn't successful, call the function post_failure */
-    .catch(post_failure);
-}
-
-/* getting the button from the page */
-let social_button = document.getElementById(`social_button`);
-/* after the button being clicked, call the function socialActivity */
-social_button.addEventListener(`click`, socialActivity);
-
-/* this function get an aleatory course from an API based on the music category */
-function musicActivity(details) {
-    /* getting the section from the page */
-    let section = document.querySelector(`section`);
-    /* this function insert onto the page the type of category was chosen, its activity, price and link the course */
-    function post_success(response) {
-      section.insertAdjacentHTML(
-        `beforeend`,
-        `<div>
-              <h1>${response[`data`][`type`]}</h1>
-              <h2>${response[`data`][`activity`]}</h2>
-              <p>$ ${response[`data`][`price`]}</p>
-              <a href=${response[`data`][`link`]}>Link to course</a>
-              </div>
-              `
-      );
-    }
-  
-    /* in case the request fail, an error message will be inserted to the page */
-    function post_failure(error) {
-      main.insertAdjacentHTML(
-        `beforeend`,
-        `
-                <h1>ERROR</h1>
-                `
-      );
-    }
-  
-    /* making request to an API already defined by its category */
-    axios
-      .request({
-        url: `http://www.boredapi.com/api/activity?type=music`,
-      })
-      /* in case the request was successful, call the function post_success */
-      .then(post_success)
-      /* in case the request wasn't successful, call the function post_failure */
-      .catch(post_failure);
-  }
-  
-  /* getting the button from the page */
-  let music_button = document.getElementById(`music_button`);
-  /* after the button being clicked, call the function socialActivity */
-  music_button.addEventListener(`click`, musicActivity);
+/* get this activity button from the page */
+let button = document.getElementById(`activity_button`);
+/* after being clicked, call the function displayActivity */
+button.addEventListener(`click`, displayActivity);
